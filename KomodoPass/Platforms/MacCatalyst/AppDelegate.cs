c595e09133a7042334e54f6316ef194829065bac
Campolim <ianreiscampolim@gmail.com>
2025-01-16 18:32:04 -0300

    Add project files.

---
+++ KomodoPass/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace KomodoPass
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
